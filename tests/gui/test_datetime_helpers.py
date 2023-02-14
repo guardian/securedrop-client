1c13b7cf22cd942cf86b07429216c2bdea5ffa90
Philip McMahon <philip.mcmahon@guardian.co.uk>
2023-02-14 15:34:03 +0000

    Add test for french locale

--- tests/gui/test_datetime_helpers.py
+++ tests/gui/test_datetime_helpers.py
@@ -3,9 +3,12 @@ import datetime
 from securedrop_client.gui.datetime_helpers import format_datetime_month_day
 
 
-def test_format_datetime_month_day():
+def test_format_datetime_month_day(mocker):
     # Dates are shown in the source list as well as the conversation view. Changing the date format may result in UI issues -
     # this test is a reminder to check both views!
-    midnight_january_london = datetime.datetime(2023, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    midnight_january_london = datetime.datetime(2023, 2, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
 
-    assert format_datetime_month_day(midnight_january_london) == "Jan 1"
+    assert format_datetime_month_day(midnight_january_london) == "Feb 1"
+
+    mocker.patch("locale.getdefaultlocale", return_value=('fr_FR', None))
+    assert format_datetime_month_day(midnight_january_london) == "févr 1"
